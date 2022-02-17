74de88c49e707fd76d9490cd7eb6a756d8450ff4
xuanphuoc92 <xuanphuoc92@gmail.com>
2022-02-17 23:15:05 +0800

    Implement Transfer

---
+++ CleanBankAccountKata/Interactors/AccountTransfer.cs
@@ -0,0 +1,26 @@
+﻿using CleanBankAccountKata.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanBankAccountKata.Interactors
+{
+    class AccountTransfer
+    {
+        private Account account;
+
+        public AccountTransfer(Account account)
+        {
+            this.account = account;
+        }
+
+        internal void Transfer(decimal amount, Account other)
+        {
+            AccountWithdraw withdraw = new AccountWithdraw(account);
+            AccountDeposit deposit = new AccountDeposit(other);
+
+            withdraw.Withdraw(amount);
+            deposit.Deposit(amount);
+        }
+    }
+}
---
+++ CleanBankAccountKata/Interactors/AccountTransfer_Test.cs
@@ -0,0 +1,29 @@
+﻿using CleanBankAccountKata.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CleanBankAccountKata.Interactors
+{
+    public class AccountTransfer_Test
+    {
+        [Fact]
+        public void Test()
+        {
+            Account account1 = new Account();
+            Account account2 = new Account();
+            AccountDeposit deposit = new AccountDeposit(account1);
+            AccountTransfer transfer = new AccountTransfer(account1);
+
+            deposit.Deposit(100.05M);
+            transfer.Transfer(20.05M, account2);
+
+            Assert.Equal(80M, account1.Balance);
+            Assert.Equal(20.05M, account2.Balance);
+
+            Assert.Equal(2, account1.Transactions.Count);
+            Assert.Single(account2.Transactions);
+        }
+    }
+}
