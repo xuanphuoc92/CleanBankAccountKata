6be8430bd9efceed08d442e19f298427e3951f0c
xuanphuoc92 <xuanphuoc92@gmail.com>
2022-02-17 23:49:52 +0800

    Refactor the behaviours in AccounContext into an instance of MemoryDataAccess

--- CleanBankAccountKata/DataInterfaces/AccountContext.cs
+++ CleanBankAccountKata/DataInterfaces/AccountContext.cs
@@ -7,25 +7,10 @@ namespace CleanBankAccountKata.DataInterfaces
 {
     class AccountContext
     {
-        private Dictionary<int, Account> keyValues = new Dictionary<int, Account>();
+        private MemoryDataAccess dataAccess = new MemoryDataAccess();
 
-        public int Count => keyValues.Count;
-
-        internal void Save(Account account)
-        {
-            if (account.Id == 0)
-                account.Id = keyValues.Count + 1;
-            if (keyValues.ContainsKey(account.Id))
-                keyValues[account.Id] = account;
-            else
-                keyValues.Add(account.Id, account);
-        }
-
-        internal Account Load(int id)
-        {
-            if (keyValues.ContainsKey(id))
-                return keyValues[id];
-            return null;
-        }
+        internal int Count => dataAccess.Count;
+        internal void Save(Account account) => dataAccess.Save(account);
+        internal Account Load(int id) => dataAccess.Load(id);
     }
 }
---
+++ CleanBankAccountKata/DataInterfaces/MemoryDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using CleanBankAccountKata.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanBankAccountKata.DataInterfaces
+{
+    class MemoryDataAccess
+    {
+        private Dictionary<int, Account> keyValues = new Dictionary<int, Account>();
+
+        internal int Count => keyValues.Count;
+
+        internal void Save(Account account)
+        {
+            if (account.Id == 0)
+                account.Id = keyValues.Count + 1;
+            if (keyValues.ContainsKey(account.Id))
+                keyValues[account.Id] = account;
+            else
+                keyValues.Add(account.Id, account);
+        }
+
+        internal Account Load(int id)
+        {
+            if (keyValues.ContainsKey(id))
+                return keyValues[id];
+            return null;
+        }
+    }
+}
