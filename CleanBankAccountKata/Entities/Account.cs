f486da60d17785252c075cd59e92a8bf31cbaac4
xuanphuoc92 <xuanphuoc92@gmail.com>
2022-02-17 22:36:59 +0800

    Implement Account

---
+++ CleanBankAccountKata/Entities/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanBankAccountKata.Entities
+{
+    class Account
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ CleanBankAccountKata/Entities/Account_Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CleanBankAccountKata.Entities
+{
+    public class Account_Test
+    {
+        [Fact]
+        public void Default()
+        {
+            Account account = new Account();
+            Assert.Equal(0, account.Id);
+            Assert.Equal(0, account.Balance);
+        }
+    }
+}
