0f588b0a77d0211a47594b70f9fb4e946ab57b58
xuanphuoc92 <xuanphuoc92@gmail.com>
2022-02-17 22:55:21 +0800

    Implement Transaction

---
+++ CleanBankAccountKata/Entities/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanBankAccountKata.Entities
+{
+    class Transaction
+    {
+        public Account Account { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime Date { get; set; } = DateTime.Today;
+    }
+}
---
+++ CleanBankAccountKata/Entities/Transaction_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CleanBankAccountKata.Entities
+{
+    public class Transaction_Test
+    {
+        [Fact]
+        public void Test()
+        {
+            Transaction transaction = new Transaction();
+            Assert.Null(transaction.Account);
+            Assert.Equal(0, transaction.Amount);
+            Assert.Equal(0, transaction.Balance);
+        }
+    }
+}
