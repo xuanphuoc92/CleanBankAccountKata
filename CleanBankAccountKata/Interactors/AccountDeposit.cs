91acb17283b5d1595d33b8ab1767bada6e7c613a
xuanphuoc92 <xuanphuoc92@gmail.com>
2022-02-17 22:42:39 +0800

    Implement AccountDeposit

---
+++ CleanBankAccountKata/Interactors/AccountDeposit.cs
@@ -0,0 +1,22 @@
+﻿using CleanBankAccountKata.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanBankAccountKata.Interactors
+{
+    class AccountDeposit
+    {
+        private Account account;
+
+        public AccountDeposit(Account account)
+        {
+            this.account = account;
+        }
+
+        internal void Deposit(int amount)
+        {
+            account.Balance += amount;
+        }
+    }
+}
---
+++ CleanBankAccountKata/Interactors/AccountDeposit_Test.cs
@@ -0,0 +1,22 @@
+﻿using CleanBankAccountKata.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CleanBankAccountKata.Interactors
+{
+    public class AccountDeposit_Test
+    {
+        [Fact]
+        public void Test()
+        {
+            Account account = new Account();
+            AccountDeposit deposit = new AccountDeposit(account);
+
+            Assert.Equal(0, account.Balance);
+            deposit.Deposit(100);
+            Assert.Equal(100, account.Balance);
+        }
+    }
+}
