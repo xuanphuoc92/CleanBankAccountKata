bb9c2ffff04e4c091d28d39135cd4b5d52f29885
xuanphuoc92 <xuanphuoc92@gmail.com>
2022-02-17 23:45:19 +0800

    Implement Account Context

---
+++ CleanBankAccountKata/DataInterfaces/AccountContext_Test.cs
@@ -0,0 +1,35 @@
+﻿using CleanBankAccountKata.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CleanBankAccountKata.DataInterfaces
+{
+    public class AccountContext_Test
+    {
+        [Fact]
+        public void Test()
+        {
+            Account account = new Account();
+            account.Transact(100.05M);
+            account.Transact(-20M);
+            Assert.Equal(0, account.Id);
+            Assert.Equal(80.05M, account.Balance);
+            Assert.Equal(2, account.Transactions.Count);
+
+            AccountContext context = new AccountContext();
+            Assert.Equal(0, context.Count);
+
+            context.Save(account);
+            Assert.Equal(1, account.Id);
+            Assert.Equal(80.05M, account.Balance);
+            Assert.Equal(2, account.Transactions.Count);
+
+            Account loadedAccount = context.Load(1);
+            Assert.Equal(1, loadedAccount.Id);
+            Assert.Equal(80.05M, loadedAccount.Balance);
+            Assert.Equal(2, loadedAccount.Transactions.Count);
+        }
+    }
+}
