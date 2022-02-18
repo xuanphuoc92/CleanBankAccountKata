bdc333323e9442443fb2ca5923547ea5a208fbc5
xuanphuoc92 <xuanphuoc92@gmail.com>
2022-02-18 22:47:00 +0800

    Create Data structures for Statement

---
+++ CleanBankAccountKata/Interactors/StatementInputData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanBankAccountKata.Interactors
+{
+    [Serializable]
+    class StatementInputData
+    {
+        public int AccountId { get; set; }
+    }
+}
---
+++ CleanBankAccountKata/Interactors/StatementOutputData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanBankAccountKata.Interactors
+{
+    [Serializable]
+    class StatementOutputData
+    {
+        public int AccountId { get; set; }
+
+        public class LineItems
+        {
+            public DateTime Date { get; set; }
+            public decimal? Credit { get; set; }
+            public decimal? Debit { get; set; }
+            public decimal Balance { get; set; }
+        }
+    }
+}
